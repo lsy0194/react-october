fd264318a1c02782ebe88d68c4c19b6b968dd185
lsy0194 <tktmadltmddu@naver.com>
2023-10-04 14:21:42 +0900

    department페이지 마운트시 on클래스 적용

--- src/components/common/layout/Layout.jsx
+++ src/components/common/layout/Layout.jsx
@@ -1,9 +1,21 @@
+import { useEffect, useRef } from 'react';
 import styles from './Layout.module.scss';
 import clsx from 'clsx';
 
 export default function Layout({ title, children, styleName }) {
+	//가상돔 요소를 document.querySelector를 쓰지않고 실시간으로 참조하고싶을때
+	//빈 참조객체를 useRef로 생성
+	const frame = useRef(null);
+	const isOn = useRef(false);
+
+	useEffect(() => {
+		//컴포넌트 마운트시 1번만 호출
+		//페이지가 변경이 되서 각페이지마다의 layout컴포넌트가 마운트될시 한번만 on클래스를 추가
+		isOn.current = true;
+	}, []);
 	return (
-		<section className={clsx(styles.layout, styleName)}>
+		//참조하고싶은 가상돔 요소에 ref로 연결
+		<section ref={frame} className={clsx(styles.layout, styleName, isOn.current ? styles.on : '')}>
 			<figure></figure>
 
 			<div className={clsx(styles.content, styleName)}>
--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -1,10 +1,11 @@
 import Layout from '../../common/layout/Layout';
 //import styles from './Department.module.scss';
 //import clsx from 'clsx';
+import styles from './Gallery.module.scss';
 
 export default function Gallery() {
 	return (
-		<Layout title={'Gallery'}>
+		<Layout title={'Gallery'} styleName={styles.gallery}>
 			<p>갤러리 페이지입니다.</p>
 		</Layout>
 	);
